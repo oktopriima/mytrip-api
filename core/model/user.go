7b37311ca5cf594589dd2b0fd429f20ab2390cb6
Okto Prima Jaya <octoprima93@gmail.com>
2018-09-06 01:11:27 +0700

    Update glide

--- core/model/user.go
+++ core/model/user.go
@@ -3,18 +3,18 @@ package model
 import (
 	"time"
 
-	"github.com/oktopriima/mytrip-api/lib"
+	"github.com/oktopriima/go-package/library"
 )
 
 type Users struct {
-	ID            int64          `json:"id"`
-	Name          string         `json:"name"`
-	Email         string         `json:"email"`
-	Password      string         `json:"password"`
-	RememberToken lib.NullString `json:"remember_token"`
-	Active        int            `json:"active"`
-	ActiveToken   lib.NullString `json:"activation_token"`
-	CreatedAt     time.Time      `json:"created_at"`
-	UpdatedAt     time.Time      `json:"updated_at"`
-	DeletedAt     lib.NullTime   `json:"deleted_at"`
+	ID            int64              `json:"id"`
+	Name          string             `json:"name"`
+	Email         string             `json:"email"`
+	Password      string             `json:"password"`
+	RememberToken library.NullString `json:"remember_token"`
+	Active        int                `json:"active"`
+	ActiveToken   library.NullString `json:"activation_token"`
+	CreatedAt     time.Time          `json:"created_at"`
+	UpdatedAt     time.Time          `json:"updated_at"`
+	DeletedAt     library.NullTime   `json:"deleted_at"`
 }
